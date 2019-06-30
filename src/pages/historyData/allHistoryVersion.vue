ed803edc49381098b15c901e03db57a476f7bfa9
wxdandelion <wangxin1231@sogou-inc.com>
2019-06-30 22:03:18 +0800

    历史告警数据可视化写死数据

--- src/pages/historyData/allHistoryVersion.vue
+++ src/pages/historyData/allHistoryVersion.vue
@@ -98,6 +98,7 @@
         this.spinShow = false;
       },
       async getClassfyData(){
+        /* 接口
         let params = {
           "sitewhereToken": localStorage.getItem("sitewhereToken"),
         };
@@ -120,9 +121,22 @@
         for (let i = 0; i < temp.length; i++) {
           temp[i].value = res.results.sumNum[i];
         }
+        */
+        classfyChart.option.series[0].data = [0, 2, 1, 7, 6, 8, 20, 1, 7, 10, 0, 1];
+        classfyChart.option.series[1].data = [7, 1, 1, 10, 5, 8, 1, 1, 5, 9, 0, 0];
+        classfyChart.option.series[2].data = [8, 11, 9, 0, 5, 1, 2, 1, 7, 3, 0, 5];
+        classfyChart.option.series[3].data = [0, 0, 1, 5, 2, 7, 2, 2, 4, 7, 1, 1];
+        classfyChart.option.series[4].data = [5, 3, 4, 1, 7, 8, 2, 4, 8, 9, 11, 0];
 
+        //饼图
+        let arr = [20, 30, 50, 11, 6];
+        let temp = classfyPie.option.series[0].data;
+        for (let i = 0; i < temp.length; i++) {
+          temp[i].value = arr[i];
+        }
       },
       async getErrorData(){
+        /*
         let params = {
           "sitewhereToken": localStorage.getItem("sitewhereToken"),
         };
@@ -144,9 +158,21 @@
         for (let i = 0; i < temp.length; i++) {
           temp[i].value = res.results.sumNum[i];
         }
+        */
+        errorChart.option.series[0].data = [20, 60, 11, 78, 66, 18, 10, 9, 24, 10, 0, 19];
+        errorChart.option.series[1].data = [10, 32, 10, 74, 69, 28, 0, 1, 17, 9, 10, 4];
+        errorChart.option.series[2].data = [30, 21, 15, 37, 56, 48, 20, 12, 22, 7, 20, 11];
+        errorChart.option.series[3].data = [11, 12, 31, 47, 26, 21, 7, 16, 17, 9, 11, 28];
+        errorChart.option.series[4].data = [30, 22, 21, 17, 16, 18, 17, 23, 29, 15, 30, 8];
 
+        let arr = [60, 90, 80, 40, 46];
+        let temp = errorPie.option.series[0].data;
+        for (let i = 0; i < temp.length; i++) {
+          temp[i].value = arr[i];
+        }
       },
       async getWarnErrorData(){
+        /*
         let params = {
           "sitewhereToken": localStorage.getItem("sitewhereToken"),
         };
@@ -165,7 +191,15 @@
         for (let i = 0; i < temp.length; i++) {
           temp[i].value = res.results.sumNum[i];
         }
+        */
+        warnErrorChart.option.series[0].data = [60, 52, 71, 87, 36, 48, 27, 63, 39, 25, 40, 18];
+        warnErrorChart.option.series[1].data = [40, 32, 21, 47, 66, 28, 57, 73, 39, 65, 10, 48];
 
+        let arr = [30, 70];
+        let temp = warnErrorPie.option.series[0].data;
+        for (let i = 0; i < temp.length; i++) {
+          temp[i].value = arr[i];
+        }
       },
 
 
